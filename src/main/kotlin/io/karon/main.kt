e5cfef21450b2fff09bec3d81a59c037fc5cff24
Arthur Caron <arthur@karon.io>
2021-02-07 21:35:49 +0100

    basic project

---
+++ src/main/kotlin/io/karon/main.kt
@@ -0,0 +1,4 @@
+package io.karon
+
+// Project idea from: https://robertheaton.com/2018/06/12/programming-projects-for-advanced-beginners-ascii-art/
+
---
+++ src/test/kotlin/io/karon/HelloTest.kt
@@ -0,0 +1,11 @@
+package io.karon
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class HelloTest {
+    @Test
+    fun test() {
+        Assertions.assertTrue(true)
+    }
+}
